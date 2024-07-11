836151089048802fe35aa24c5a915855e6af7398
unknown <mel.zuercher@gmail.com>
2024-07-11 17:02:04 +0200

    assertion correction for factorial preventing inf loops

--- algorithms/recursion/factorial.py
+++ algorithms/recursion/factorial.py
@@ -2,11 +2,13 @@
 # recursive Factorial
 
 def factorial(n:int)->int:
-    assert isinstance(n,int),TypeError
+    assert isinstance(n,int)&(n>=0),TypeError("Invalid input. Make sure it is a non-negative integer.")
     if n==0:
         return 1
     else:
         return n*factorial(n-1)
-    
+
+"""
 for i in range(10):
     print(factorial(i))
+    """
