211a78d5ed40c178e02ec79ac53365d0b7860473
Mel Zurcher <mel.zuercher@gmail.com>
2024-08-24 09:51:51 +0200

    better design of merge_sort code

--- algorithms/sorting/test_and_compare_sorts.py
+++ algorithms/sorting/test_and_compare_sorts.py
@@ -8,8 +8,8 @@ l=list(range(100000))
 l_permutation=l.copy()
 random.shuffle(l_permutation)
 #print(l_permutation)
+#print(l)
 results=[merge_sort(l_permutation),
-         quick_sort(l_permutation)]
-         
+         quick_sort(l_permutation)]      
 equality_test=[result==l for result in results]        
 print(equality_test)
